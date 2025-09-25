c27ee2e33df94cd971d22ad52892b490ac332929
Jakub Kosiorek <jakkos69@gmail.com>
2025-09-25 19:07:57 +0200

    yeah, this is some casual commit before changing os

--- bookmarks.sh
+++ bookmarks.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-USAGE="usage: m [-ar] <flag name>"
-UNRECOGNIZED_FLAG="flag $1 is not recognized"
-UNRECOGNIZED_ARG="argument $1 is not recognized"
+usage="usage: m [-ar] <flag name>"
+unrecognized_flag="flag $1 is not recognized"
+unrecognized_arg="argument $1 is not recognized"
 
 PRINT_DEBUG=1
 
 #ensure there is input provided
-[[ -z "$1" ]] && echo "no arguments were provided" && echo $USAGE
+[[ -z "$1" ]] && echo "no arguments were provided" && echo $usage
 
 change_current_path()
 {
 	local path="$1"
 	echo "jumping to: $path"
-	[[ -z "$1" ]] && path="/Users/jk"
+	[[ -z "$1" ]] && path="/home/jk"
 	[[ -n "$2" ]] && echo "too many arguments provided" && return
 	echo "jumping to: $path"
 	cd $path
@@ -21,8 +21,8 @@ change_current_path()
 
 jump_to_bookmark()
 {
-	local file_name="/Users/jk/temp/bookmark_locations"
-	local file_location="/Users/jk/temp"
+	local file_name="/home/jk/temp/bookmark_locations"
+	local file_location="/home/jk/temp"
 	local bookmark_name="$1"
 	#echo $bookmark_name
 	#echo $file_name
@@ -40,8 +40,8 @@ create_new_bookmark()
 {
 	local path="$(pwd)"
 	local bookmark_name="$1"
-	local file_name="/Users/jk/temp/bookmark_locations"
-	local file_location="/Users/jk/temp"
+	local file_name="/home/jk/temp/bookmark_locations"
+	local file_location="/home/jk/temp"
 
 	# check if location + file exist
 	if [[ ! -e $file_name ]]; then
@@ -70,10 +70,10 @@ create_new_bookmark()
 case $1 in
 	'-a') echo "create flag called" && create_new_bookmark "$2";;
 	'-r') echo "remove flag called";;
-	'-test') change_current_path "/Users/jk/";;
+	'-test') change_current_path "/home/jk";;
 	'-test2') create_new_bookmark "$2";;
 	'-test3') jump_to_bookmark "$2";;
-	'-'*) echo "$UNRECOGNIZED_FLAG";;
+	'-'*) echo "$unrecognized_flag";;
 	*)	  jump_to_bookmark "$1";;
 esac
 
